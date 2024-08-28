e760c665a8f675d9fa371d69e96d5dedf6adfd08
Daniel James <daniel@onlyguera.com>
2024-08-28 13:08:44 +0200

    Update redirect.tsx

--- frontend/src/pages/redirect.tsx
+++ frontend/src/pages/redirect.tsx
@@ -4,7 +4,7 @@ const RedirectPage = () => {
   return (
     <div className="h-screen p-2 justify-center top-4 w-full text-center flex flex-col items-center">
       <img src="/check-instant.png" alt="Instant Payments" className="h-10 invert rounded-full" />
-      <h2 className="text-3xl font-bold mt-6">£1.99</h2>
+      <h2 className="text-3xl font-bold mt-6">£0.10</h2>
       <p className="text-[#333333]">to All Ears Audio</p>
       <p className="text-[#333333] text-center max-w-[350px] mt-6">Your payment of was successful.</p>
       <p className="text-[#333333] text-center max-w-[350px] mt-2">You may now safely close this tab.</p>
